b1c3531060095abf8872acdf7fc3d360a4ea11f6
jachapin <32234320+jachapin@users.noreply.github.com>
2020-11-09 00:13:22 -0600

    Create LuckyNumberGenerator.cs

---
+++ LuckyNumberGenerator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace LuckyNumberGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string intro = @"Lucky Number Generator
+How many numbers do you want?";
+            Console.WriteLine(intro);
+            int input = int.Parse(Console.ReadLine());
+
+            Random rand = new Random();
+
+            for (int i = 0; i < input; i++)
+            {
+                // Print with format string: five spaces and number type with zero decimal points
+                Console.Write("{0,5:N0}", rand.Next(1, 100));
+            }
+
+        }
+    }
+}
