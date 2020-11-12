0f52447895a8629412a8cd2a29aaeaece6ed3985
jachapin <32234320+jachapin@users.noreply.github.com>
2020-11-11 23:03:33 -0600

    Create TextToFile.cs

---
+++ TextToFile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+
+namespace TextToFile
+{
+    class TextToFile
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your to-do list:");
+
+            string input = Console.ReadLine();
+
+            // Folder must already exist on machine. Program will only create the text file
+            File.WriteAllText(@"C:\Users\Public\TestFolder\Text.txt", input);
+        }
+    }
+}
